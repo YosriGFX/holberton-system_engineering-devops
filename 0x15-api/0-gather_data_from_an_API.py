349fef20d2699c7aa5a72061d70196b982e9ecb0
YosriGFX <YosriG1997@gmail.com>
2020-07-06 15:19:44 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+Python script that, using this REST API,
+for a given employee ID, returns
+information about his/her RequestData list progress.
+'''
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    UID = argv[1]
+    Link = "https://jsonplaceholder.typicode.com/users/{}".format(
+        UID
+    )
+    user = requests.get(
+        Link,
+        verify=False
+    ).json()
+    Link = "https://jsonplaceholder.typicode.com/RequestDatas\
+        ?userId={}".format(
+        UID
+    )
+    RequestData = requests.get(
+        Link,
+        verify=False
+    ).json()
+    CTasks = []
+    for currect_Task in RequestData:
+        print(currect_Task)
+        if currect_Task.get("completed"):
+            CTasks.append(currect_Task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(CTasks), len(RequestData)))
+    for currect_Task in CTasks:
+        print("\t {}".format(currect_Task))
